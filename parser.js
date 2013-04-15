ecb3313420f8f895972d15f577c2b8d2a230dcf6
Hack Reactor Students <accounts@hackreactor.com>
2013-04-14 19:48:07 -0700

    updating to work with .txt file

--- parser.js
+++ parser.js
@@ -1,10 +1,24 @@
 //using node.js i want to open and parse email into huge object
+var fs = require("fs");
+
+var fs = require("fs");
+fs.readFile(__dirname + '/email.txt', function read(err, data) {
+  if (err) { throw err; }
+  var events = divideEvents(data.toString())
+  // console.log(events);
+});
+
 var input = "apr  5 fri Dustonious Maximus, The Skunkadelics, David's Birthday!,\n       The Thirsty Three, DetachDolls, Demi And The Gods!\n       at 924 Gilman Street, Berkeley a/a $10 7pm *** @ (Luna's 16th Birthday)\napr  5 fri Condition, Replica, Negative Standards, Stressors\n       at the Hidden Temple, 1209 8th Avenue, Oakland a/a $5 7:30pm ** @\napr  5 fri Rusty Zinn And His Band\n       at Cafe Rande Vu, 2430 Broadway, Oakland a/a free 6pm/8pm **\napr  5 fri Syd The Kyd, Trev Case, Koslov\n       at the New Parish, Oakland 18+ $10/$12 8pm/9pm ***\napr  5 fri Galaxy Express (Seoul, Korea), Unko Atama, Modern Kicks,\n       Pleasure Gallows at Eli's Mile High Club, Oakland 21+ $7 **\napr  5 fri The Mallard, Freddie And The Aztecs, The Spyrals, Steel Cranes\n       at the Uptown, Oakland 21+ free 6pm/9pm **\napr  5 fri Soilwork, Jeff Loomis, Blackguard, Hatchet\n       at Slim's, S.F. 6+ $21/$24 7pm/8pm *** @\napr  5 fri Bayonics, My Peoples, Sean Tabor, Shawn Megofna\n       at the Great American Music Hall, S.F. 6+ $15 8pm/9pm **\napr  5 fri Alesso at the Warfield, S.F. a/a # **\napr  5 fri Thee Oh Sees at the Verdi Club, 2424 Mariposa, S.F. a/a **\napr  5 fri Moonshine Cabaret at the Chapel, S.F. a/a $15/$18 9pm **\napr  5 fri Whiskerman, Decker, Kelly McFarling\n       at the Bottom of the Hill, S.F. 21+ $10 8:30pm/9pm **\napr  5 fri Billy Cramer And Share The Land, The Boars,\n       TV Mike And The Scarecrows at the Hemlock, S.F. 21+ $7 9:30pm **"
 //takes an input string and returns an array of all events
-var divideEvents = function(input){
+var divideEvents = function(subInput){
   var month =  /(?:\n)(\w{3})(?: )/;
-  var evnt, subInput = input, database = [];
+  var evnt, database = [];
 
+  //remove top and bottom from email
+  subInput = subInput.split('\n\n')[3];
+  // console.log(subInput);
+
+  //now remove all events and parse them
   while(subInput.length>0){
     evnt = {};
     evnt.txt = subInput.split(month,1).toString();
@@ -21,15 +35,21 @@ var divideEvents = function(input){
 // location, possible parameters: price, ages, will sell out, reccommended, extra $ for underage
 var eventParse = function(evnt){
   var month = /^(\w{3})(?: )/;
-  var day = /^(?:\s?)\d{1,2}(?: )/;
+  var date = /^(?:\s?)\d{1,2}(\/\d{1,2}){0,4}( postponed:  )?(?: *)/;
   var artists = / at /;
+  var dayOfWeek = /((mon)|(tue)|(wed)|(thr)|(fri)|(sat)|(sun))(?: )/;
   //filter date of event
   evnt.month = month.exec(evnt.txt)['1'];
-  evnt.txt = evnt.txt.slice(evnt.month.length+1);
-  evnt.day = day.exec(evnt.txt)[0];
-  evnt.txt = evnt.txt.slice(evnt.day.length);
-  evnt.dayOfWeek = month.exec(evnt.txt)['1'];
-  evnt.txt = evnt.txt.slice(evnt.day.length+1);
+  console.log(evnt.txt)
+  evnt.txt = evnt.txt.slice(evnt.month.length+2);
+  console.log(evnt.txt)
+  evnt.date = date.exec(evnt.txt)[0];
+  evnt.txt = evnt.txt.slice(evnt.date.length);
+  evnt.dayOfWeek = dayOfWeek.exec(evnt.txt);
+  if(evnt.dayOfWeek){
+    evnt.dayOfWeek = evnt.dayOfWeek[1];
+    evnt.txt = evnt.txt.slice(evnt.dayOfWeek.length+1);
+  }
   //filter artists field
   var artistsInd = (artists.exec(evnt.txt)).index;
   evnt.artists = evnt.txt.slice(0, artistsInd);
@@ -47,7 +67,7 @@ var eventParse = function(evnt){
   //get price and time
   evnt.price = evnt.txt.split(' ',1)[0];
   evnt.txt = evnt.txt.slice(evnt.price.length + 1);
-  evnt.time = /(\d){0,3}(am|pm)/.exec(evnt.txt);
+  evnt.time = /.*(am|pm)/.exec(evnt.txt);
   if(evnt.time) {
     evnt.time = evnt.time[0];
     evnt.txt = evnt.txt.slice(evnt.time.length +1);
@@ -85,4 +105,3 @@ var eventParse = function(evnt){
   return evnt;
 };
 
-divideEvents(input);
