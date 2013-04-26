0a0c847c523d7c5aa042a93f52ded3d5b5df0494
Selby <selby.e.walker@gmail.com>
2013-04-26 15:10:07 -0700

    changes to api

--- node_server/index.js
+++ node_server/index.js
@@ -42,52 +42,30 @@ app.use(function(req, res, next) {
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/../public'));
 
-app.get('/api/events/all', function(req,res){
-  fs.readFile(__dirname + '/../public/parsedList.json', function(err, data){
-  	if(err){ throw err; }
-    res.set("content-type", "application/json");
+app.get('/api/events?', function(req,res){
+  console.log(req.query)
+  if(req.query.all){
+    fs.readFile(__dirname + '/../public/parsedList.json', function(err, data){
+    	if(err){ throw err; }
+      res.set("content-type", "application/json");
 
-    res.json(JSON.parse(data.toString()));
-  });
-});
-
-//find events by id
-app.get('/api/events/id:dat',function(req,res){
-  var id = req.params.dat.substring(1);
-  console.log('Retrieving event: ' + id);
-    Event.findOne({'_id': id}, function(err, item) { res.send(item); });
-});
-
-//find events by date
-app.get('/api/events/date:dat',function(req,res){
-  var date = req.params.dat.substring(1);
-  console.log('Retrieving event on date: ' + date);
-    Event.find({'date': date}, function(err, item) { res.send(item); });
-});
-
-//find events that will sell out
-app.get('/api/events/willSellout',function(req,res){
-  console.log('Retrieving events that will sell out');
+      res.json(JSON.parse(data.toString()));
+    });
+  } else if(req.query.date) {
+    console.log('Retrieving event on date: ' + req.query.date);
+    Event.find({'date': req.query.date}, function(err, item) { res.send(item); });
+  } else if(req.query.willSellout) {
+    console.log('Retrieving events that will sell out');
     Event.find({'willSellout': true}, function(err, item) { res.send(item); });
+  } else if(req.query.venue) {
+    console.log('Retrieving events at venue: ' + req.query.venue);
+    Event.find({'venue': req.query.venue}, function(err, item) { res.send(item); });
+  } else if(req.query.artist) {
+    console.log('Retrieving events by artist: ' + req.params.artist);
+    Event.find({'artists': req.query.artist}, function(err, item) { res.send(item); });
+  } 
 });
 
-//find events by artist name
-app.get('/api/events/artist:dat',function(req,res){
-  var artist = req.params.dat.replace('+',' ').substring(1); //TODO: url decode
-  console.log('Retrieving events by artist: ' + artist);
-    Event.find({'artists': artist}, function(err, item) { res.send(item); });
-});
-
-//find events by venue name
-app.get('/api/events/venue:dat',function(req,res){
-  var venue = req.params.dat.replace('+',' ').substring(1);
-  console.log('Retrieving events at venue: ' + venue);
-    Event.find({'venue': venue}, function(err, item) { res.send(item); });
-});
-
-
-//routes
-// app.get('')
 
 
 var port = process.env.PORT || 3000;
