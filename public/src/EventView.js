f8c407283c61deefb887409f42f9237f69ca62f9
Selby <selby.e.walker@gmail.com>
2013-04-25 18:06:54 -0700

    additional ui changes

--- public/src/EventView.js
+++ public/src/EventView.js
@@ -35,11 +35,28 @@ var ArtistEventView = Backbone.View.extend({
     },
 
 	fullRender: function(){
+		console.log(this)
 		var html = '<div href="#' + this.model.get('cid') + '"" class="well evnt"><ul>';
 		html += '<h4 href="#" class venue>' + this.model.get('venue') + '</h4>';
-    	html += '<span class="fui-man-24"></span> ' + this.model.get('artists') + '<br/>';
+    	html += '<span class="fui-volume-24"></span> ' + this.model.get('artists') + '<br/>';
     	html += '<span class="fui-location-24"></span> ' + this.model.get('address') + '<br/>';
-
+    	if(this.model.get('ages')) {
+    		html += '<span class="fui-man-24"></span> ' + this.model.get('ages') + '<br/>';
+    	}
+    	if(this.model.get('priceAndTime') && this.model.get('priceAndTime').length > 0) {
+    		html += '<span class="fui-time-24"></span> ' + this.model.get('priceAndTime') + '<br/>';
+    	}
+    	if(this.model.get('recommended')){
+    		for(var i = 0; i < this.model.get('recommended'); i++) {
+    			html += '<span class="fui-heart-16"></span> '
+    		}
+    	}
+    	if(this.model.get('willSellout')) html += 'Sellout Risk ';
+    	if(this.model.get('underagePayMore')) html += 'Underage Pay More ';
+    	if(this.model.get('pitWarning')) html += 'Pit Warning ';
+    	if(this.model.get('noInsOuts')) html += 'No Ins and Outs ';
+    	if(this.model.get('specialInfo')) html += this.model.get('specialInfo');
+    	
     	return this.$el.html(html += '</ul></div>');
   	},
 
