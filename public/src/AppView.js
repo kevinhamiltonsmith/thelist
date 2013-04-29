d6eb372bb1b2b5ecc9b8c0e4468f6dbdea3904a5
Selby <selby.e.walker@gmail.com>
2013-04-29 11:36:58 -0700

    list.html moved to index.html

--- public/src/AppView.js
+++ public/src/AppView.js
@@ -1,5 +1,6 @@
 var AppView = Backbone.View.extend({
 
+	
   initialize: function(params){
     this.EventsView = new EventsView({model: this.model.get('events')});
     this.model.on('remove add', function(model){ this.EventsView.render(); }, this);
