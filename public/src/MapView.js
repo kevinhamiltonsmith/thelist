b40f522b1a1a6923740608be34f6e7ef81b83110
Selby Walker <selby.e.walker@gmail.com>
2013-05-21 17:32:21 -0700

    map update

--- public/src/MapView.js
+++ public/src/MapView.js
@@ -1,6 +1,5 @@
 var MapView = Backbone.View.extend({
 
-  
   initialize: function(params){
     thisMap = L.map('map', {
                 center: [37.783548, -122.408953],
@@ -9,12 +8,29 @@ var MapView = Backbone.View.extend({
     L.tileLayer('http://b.tile.cloudmade.com/89c89b090746449a9acbd91872dd7351/1930/256/{z}/{x}/{y}.png', {
       maxZoom: 18
       }).addTo(thisMap);
+  },
 
-    this.model.on('remove add', function(model){ console.log('yo'); L.redraw(); }, this);
+  events: {
+    'submit .searchbox': function(e) {
+      e.preventDefault();
+      this.collection.filterSearch($('.textbox').val());
+    },
+    'keyup .searchbox': function(e) {
+      e.preventDefault();
+      console.log('yo')
+      if(window.prevTimeout) {
+        window.clearTimeout(window.prevTimeout);
+      }
+      var that = this;
+      window.prevTimeout = setTimeout(function(){
+        that.collection.filterSearch($('.textbox')[0].value);
+      }, 600);
+    }
   },
 
   render: function(){
     var that = this;
+    $('.searchbox').html('<form class="searchbox"><input class="textbox" type="text" placeholder="eg: 944 Market St, SF"></form>');
     $('.map').html(new GoogleMapView({collection: this.model.get('events')}).render());
 
     $('#reportrange').daterangepicker({
@@ -33,9 +49,38 @@ var MapView = Backbone.View.extend({
         if(that.model.get('events').models) {
           that.model.get('events').models.forEach(function(evnt){
             if(evnt.marker) thisMap.removeLayer(evnt.marker);
-          })
+          });
         }
       }
     );
+
+    $('.textbox').on('keyup', function(e){
+      e.preventDefault();
+      if(window.prevTimeout) {
+        window.clearTimeout(window.prevTimeout);
+      }
+      window.prevTimeout = setTimeout(function(){
+        // that.model.get('events').filterSearch($('.textbox').val());
+        $.ajax({
+          url: 'http://nominatim.openstreetmap.org/search',
+          data: { q : $('.textbox').val(), format: 'json'}
+        }).done(function(data){
+          if(data[0]) {
+            thisMap.panTo(new L.LatLng(data[0].lat, data[0].lon));
+          }
+        });
+      }, 600);
+    });
+    $('.searchbox').on('submit', function(e){
+      e.preventDefault();
+      $.ajax({
+        url: 'http://nominatim.openstreetmap.org/search',
+        data: { q : $('.textbox').val(), format: 'json'}
+      }).done(function(data){
+        if(data[0]) {
+          thisMap.panTo(new L.LatLng(data[0].lat, data[0].lon));
+        }
+      });
+    });
   }
 });
